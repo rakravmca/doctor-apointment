6ddee848d59410aca622ca0c43cb03893d8eca99
Rakesh Nayak <rakeshn@mindfiresolutions.com>
2020-07-20 21:27:38 +0530

    Virtual columns

--- src/models/Doctor.js
+++ src/models/Doctor.js
@@ -6,6 +6,11 @@ const doctorSchema = mongoose.Schema({
         required: true,
         trim: true
     },
+    specialist: {
+        type: String,
+        required: true,
+        trim: true
+    },
     user: {
         type: mongoose.Types.ObjectId,
         ref: 'User',
@@ -26,7 +31,39 @@ const doctorSchema = mongoose.Schema({
         required: true,
         trim: true
     },
-})
+}, 
+{
+    toObject: {
+        virtuals: true
+    },
+    toJSON: {
+        virtuals: true 
+    }
+});
+
+doctorSchema.virtual('start_time').get(function() {
+    if(this.day_start_time >= 12)
+    {
+        return (this.day_start_time - 12) + ':00 pm'
+    }
+    else{
+        return this.day_start_time + ':00 am'
+    }
+});
+
+doctorSchema.virtual('end_time').get(function() {
+    if(this.day_end_time >= 12)
+    {
+        return (this.day_end_time - 12) + ':00 pm'
+    }
+    else{
+        return this.day_end_time + ':00 am'
+    }
+});
+
+doctorSchema.virtual('patients_per_day').get(function() {
+    return parseInt(((this.day_end_time - this.day_start_time) * 60) / this.appointment_slot_time)
+});
 
 const Doctor = mongoose.model('Doctor', doctorSchema)
 
